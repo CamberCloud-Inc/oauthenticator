77fd0e22d58cb1be57a9e96bfb6e80e07e2e81ec
Chen Chen <cinspire93@gmail.com>
2023-04-15 22:04:07 -0700

    modify camber oauth

--- oauthenticator/camber.py
+++ oauthenticator/camber.py
@@ -6,21 +6,6 @@ import requests
 
 class CamberOAuthenticator(GenericOAuthenticator):
 
-    async def update_auth_model(self, auth_model):
-        user_info = auth_model["auth_state"][self.user_auth_state_key]
-        if callable(self.claim_groups_key):
-            groups = self.claim_groups_key(user_info)
-        else:
-            groups = user_info.get(self.claim_groups_key)
-
-        if groups:
-            # Add group information to auth_model
-            auth_model["groups"] = groups
-            auth_model['admin'] = self.check_user_in_groups(
-                groups, self.admin_groups
-            )
-        return auth_model
-
     def pre_spawn_start(self, user, spawner):
         auth_site_url = os.environ.get("AUTH_SITE_URL")
         auth_api_key = os.environ.get("AUTH_API_KEY")
@@ -37,3 +22,50 @@ class CamberOAuthenticator(GenericOAuthenticator):
                 spawner.environment["CAMBER_GROUP"] = groups.pop(0)
         else:
             self.log.warning("No `AUTH_SITE_URL` or `AUTH_API_KEY` set for CamberHub")
+
+    async def authenticate(self, handler, data=None):
+        code = handler.get_argument("code")
+
+        params = dict(
+            redirect_uri=self.get_callback_url(handler),
+            code=code,
+            grant_type='authorization_code',
+        )
+        params.update(self.extra_params)
+
+        headers = self._get_headers()
+
+        token_resp_json = await self._get_token(headers, params)
+
+        user_data_resp_json = await self._get_user_data(token_resp_json)
+
+        if callable(self.username_key):
+            name = self.username_key(user_data_resp_json)
+        else:
+            name = user_data_resp_json.get(self.username_key)
+            if not name:
+                self.log.error(
+                    "OAuth user contains no key %s: %s",
+                    self.username_key,
+                    user_data_resp_json,
+                )
+                return
+
+        user_info = {
+            'name': name,
+            'auth_state': self._create_auth_state(token_resp_json, user_data_resp_json),
+        }
+
+        if callable(self.claim_groups_key):
+            groups = self.claim_groups_key(user_data_resp_json)
+        else:
+            groups = user_data_resp_json.get(self.claim_groups_key)
+
+        if groups:
+            # Add group information to auth_model
+            user_info["groups"] = groups
+            user_info["admin"] = self.check_user_in_groups(
+                groups, self.admin_groups
+            )
+
+        return user_info
